8ee435faed2e694913f659dd4e46ed3d703c6349
Brandon Wood <bwood02@byu.edu>
2026-01-27 22:20:54 -0700

    added another class yay

---
+++ AnotherClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitHubFun
+{
+    internal class AnotherClass
+    {
+        public void AnotherMethod()
+        {
+            Console.WriteLine("This is another method in AnotherClass.");
+        }
+    }
+}
